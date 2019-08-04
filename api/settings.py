0baeddda7b89718dd007ee9c319c492dbe198910
arivumatelabs <arivu@matelabs.in>
2019-08-04 19:24:52 +0530

    add

--- api/settings.py
+++ api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from datetime import timedelta
 from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -159,3 +160,6 @@ STATIC_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import django_heroku
+django_heroku.settings(locals())
