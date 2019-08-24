54a6412191cba026df1fb4a32cea9206a268d860
arivumatelabs <arivu@matelabs.in>
2019-08-25 02:34:26 +0530

    Pagination added

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
 )
 urlpatterns = [
 	path('api/feed/', views.feed_data),
-	url(r'^api/branch/(?P<bank_name>[\w|\W]+)/$', views.bank_list.as_view(), name='bank_list'),
+	url(r'^api/bank/(?P<ifsc>[\w|\W]+)/$', views.bank_list.as_view(), name='bank_list'),
 	url(r'^api/branch/(?P<bank_name>[\w|\W]+)/(?P<city>[\w]+)/$', views.branch_list.as_view(), name='branch_list'),
 	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 	path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
