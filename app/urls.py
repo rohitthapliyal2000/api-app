9895e76789e20ec5a8abe877666347985ef04a40
arivumatelabs <arivu@matelabs.in>
2019-08-24 14:42:30 +0530

    add

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
 	path('api/feed/', views.feed_data),
 	path('api/bank/<slug:ifsc>', views.bank_list.as_view(), name='bank_list'),
-	path('api/branch/(?P<bank_name>[\\w|\\W]+)/<slug:city>', views.branch_list.as_view(), name='branch_list'),
+	path(r'^api/branch/(?P<bank_name>[\w|\W]+)/$', views.branch_list.as_view(), name='branch_list'),
 	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 	path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
